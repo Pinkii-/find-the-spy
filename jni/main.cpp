69afa36b0dae1578ff8706cbab2775b3a492b15c
Pinkii- <pinkiibiza@gmail.com>
2016-11-04 19:45:30 +0100

    Add almost the gameloop

--- jni/main.cpp
+++ jni/main.cpp
@@ -7,7 +7,20 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, const char* argv[]){
-    
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Yuegal");
     srand (time(NULL));
 
+    sf::Clock clock;
+    while (window.isOpen()) {
+      float deltaTime = clock.restart().asSeconds();
+
+      sf::Event event;
+      while (window.pollEvent(event)) {
+        switch (event.type) {
+          case sf::Event::Closed:
+            exit(0);
+            break;
+        }
+      }
+    }
 }
