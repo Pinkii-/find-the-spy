656015497261e08f1319735c8dcaca74d2dcd107
Kaito <kaaitokid@gmail.com>
2016-11-04 21:49:41 +0100

    added res and timer

--- jni/Resources.cpp
+++ jni/Resources.cpp
@@ -1,8 +1,15 @@
 #include "Resources.hpp"
-
+#include <iostream>
+#define TEXTUREPATH "res/pics"
 // sf::Texture        Resources::....
-
+sf::Font Resources::font;
+sf::Texture Resources::OKButton;
+sf::Texture Resources::PressedOKButton;
 
 void Resources::load() {
     //...loadFromFile();
+    if (!font.loadFromFile("res/fonts/font.otf")) std::cout << "penefont" << std::endl;
+
+    if(! OKButton.loadFromFile (TEXTUREPATH+std::string("buttons/OKButton.png")) ) std::cout << "okbutton not loaded" << std::endl;
+    if(! PressedOKButton.loadFromFile (TEXTUREPATH+std::string("buttons/PressedOKButton.png")) ) std::cout << "pressedokbutton not loaded" << std::endl;
 }
--- jni/Resources.hpp
+++ jni/Resources.hpp
@@ -1,9 +1,17 @@
 #ifndef __RESOURCES_HPP__
 #define __RESOURCES_HPP__
 
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 class Resources {
 public:
     static void load();
+    static sf::Font font;
+    static sf::Texture OKButton;
+    static sf::Texture PressedOKButton;
     //static sf::Texture ...
 private:
 
