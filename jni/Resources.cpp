29bf115cfa4830d4cfd64fc459c4c5eabe7b5888
Kaito <kaaitokid@gmail.com>
2016-11-05 01:06:47 +0100

    riperino and resources loaded (?)

--- jni/Resources.cpp
+++ jni/Resources.cpp
@@ -1,10 +1,21 @@
 #include "Resources.hpp"
 #include <iostream>
-#define TEXTUREPATH "res/pics"
+#define TEXTUREPATH "res/pics/"
 // sf::Texture        Resources::....
 sf::Font Resources::font;
+
 sf::Texture Resources::OKButton;
 sf::Texture Resources::PressedOKButton;
+
+sf::Texture Resources::Button;
+sf::Texture Resources::ButtonPress;
+
+sf::Texture Resources::Riperino;
+    
+sf::Texture Resources::LeftArrow;
+sf::Texture Resources::RightArrow;
+    
+sf::Texture Resources::Spy;
 
 std::vector<sf::Texture> Resources::PlayerTextures;
 
@@ -14,4 +25,14 @@ void Resources::load() {
 
     if(! OKButton.loadFromFile (TEXTUREPATH+std::string("buttons/OKButton.png")) ) std::cout << "okbutton not loaded" << std::endl;
     if(! PressedOKButton.loadFromFile (TEXTUREPATH+std::string("buttons/PressedOKButton.png")) ) std::cout << "pressedokbutton not loaded" << std::endl;
+
+    if(! Button.loadFromFile (TEXTUREPATH+std::string("buttons/Button.png")) ) std::cout << "startbuton not loaded" << std::endl;
+    if(! ButtonPress.loadFromFile (TEXTUREPATH+std::string("buttons/ButtonPress.png")) ) std::cout << "startbuttonpress not loaded" << std::endl;
+
+    if(! Riperino.loadFromFile (TEXTUREPATH+std::string("riperino.png")) ) std::cout << "riperino not loaded" << std::endl;
+
+    if(! LeftArrow.loadFromFile (TEXTUREPATH+std::string("arrowleft.png")) ) std::cout << "arrowleft not loaded" << std::endl;
+    if(! RightArrow.loadFromFile (TEXTUREPATH+std::string("arrowright.png")) ) std::cout << "arrowright not loaded" << std::endl;
+
+    if(! Spy.loadFromFile (TEXTUREPATH+std::string("spy.png")) ) std::cout << "spy not loaded" << std::endl;
 }
--- jni/Resources.hpp
+++ jni/Resources.hpp
@@ -10,8 +10,19 @@ class Resources {
 public:
     static void load();
     static sf::Font font;
+
     static sf::Texture OKButton;
     static sf::Texture PressedOKButton;
+
+    static sf::Texture Button;
+    static sf::Texture ButtonPress;
+    
+    static sf::Texture Riperino;
+    
+    static sf::Texture LeftArrow;
+    static sf::Texture RightArrow;
+    
+    static sf::Texture Spy;
     
     static std::vector<sf::Texture> PlayerTextures;
     //static sf::Texture ...
